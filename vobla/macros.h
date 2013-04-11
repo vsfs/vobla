e8f0a70f7702c9ddf3ccbe89a142b9de54cd8ea1
Lei Xu <eddyxu@gmail.com>
2013-04-11 14:08:51 -0500

    + Added apache license

--- vobla/macros.h
+++ vobla/macros.h
@@ -1,17 +1,29 @@
 /**
- * \brief defines widely used macros
+ * Copyright 2011-2013 (c) Lei Xu <eddyxu@gmail.com>
  *
- * Copyright 2011 (c) Lei Xu <eddyxu@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * \brief defines widely used macros
  */
 
 #ifndef VOBLA_MACROS_H_
 #define VOBLA_MACROS_H_
 
-#define DISABLE_COPY_AND_ASSIGN(ClassName) \
+#define DISALLOW_COPY_AND_ASSIGN(ClassName) \
   ClassName(const ClassName&); \
   ClassName& operator=(const ClassName&);
 
-#define DISABLE_IMPLICIT_CONSTRUCTORS(ClassName) \
+#define DISALLOW_IMPLICIT_CONSTRUCTORS(ClassName) \
   ClassName(); \
   DISABLE_COPY_AND_ASSIGN(ClassName)
 
