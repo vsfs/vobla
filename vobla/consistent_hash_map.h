eb6273e5247475edf2acb26107f31dcb0e55b475
Ziling Huang <hzlgis@gmail.com>
2013-05-07 15:09:06 -0500

    + added consistent hash map

---
+++ vobla/consistent_hash_map.h
@@ -0,0 +1,406 @@
+/*
+ * Copyright 2011-2013 (c) Ziling Huang <hzlgis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * \file vobla/consistent_hash_map.h
+ *
+ * \brief A generic class for Consistent Hashing
+ *
+ */
+
+#ifndef VOBLA_CONSISTENT_HASH_MAP_H_
+#define VOBLA_CONSISTENT_HASH_MAP_H_
+
+#include <glog/logging.h>
+#include <limits>
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+#include "vobla/map_util.h"
+#include "vobla/status.h"
+
+using std::numeric_limits;
+using std::string;
+using std::pair;
+using std::vector;
+
+namespace vobla {
+
+/**
+ * \class ConsistentHashMap
+ *
+ * \brief A generic class for Consistent Hashing, client are responsible of
+ *  calculating the key value when lookup an object.
+ *
+ * \note This class is not thread-safe.
+ */
+template <typename Key, typename Value, size_t Partitions = 1>
+class ConsistentHashMap {
+  typedef std::map<Key, Value> HashMap;
+
+ public:
+  typedef Key key_type;
+
+  typedef Value value_type;
+
+  // a pair of range <start, end>
+  typedef pair<Key, Key> pair_type;
+
+  // a pair of Value and its range
+  typedef pair<Value, pair_type> range_type;
+
+  typedef typename HashMap::iterator iterator;
+
+  typedef typename HashMap::const_iterator const_iterator;
+
+  typedef vector<Key> partition_type;
+
+  explicit ConsistentHashMap(size_t num_partitions = Partitions)
+      : num_partitions_per_node_(num_partitions) {
+  }
+
+  ConsistentHashMap(const ConsistentHashMap &rhs) {
+    *this = rhs;
+  }
+
+  virtual ~ConsistentHashMap() {
+  }
+
+  ConsistentHashMap& operator=(const ConsistentHashMap& rhs) {
+    ring_ = rhs.ring_;
+    num_partitions_per_node_ = rhs.num_partitions_per_node_;
+    return *this;
+  }
+
+  /**
+   * \brief Insert a node with a client specified key value to the hash map.
+   */
+  Status insert(const Key& key, const Value& value) {
+    if (contain_key(ring_, key)) {
+      return Status(-EEXIST, "The key is already inserted");
+    }
+    static Key kMax = numeric_limits<Key>::max();
+    for (size_t i = 0; i < num_partitions_per_node_; i++) {
+      // TODO(Ziling): improve the precision of this calculation!
+      Key new_key = (key + (kMax / num_partitions_per_node_ * i)) % kMax;
+      ring_[new_key] = value;
+    }
+    return Status::OK;
+  }
+
+  /**
+   * \brief Remove a node with a client specified key value, note this key
+   * value must be the same with the original key value when inserting.
+   */
+  Status remove(const Key& key) {
+    if (!contain_key(ring_, key)) {
+      return Status(-ENOENT, "The key does not exist");
+    }
+    static Key kMax = numeric_limits<Key>::max();
+    for (size_t i = 0; i < num_partitions_per_node_; i++) {
+      Key new_key = (key + (kMax / num_partitions_per_node_ * i)) % kMax;
+      ring_.erase(new_key);
+    }
+    return Status::OK;
+  }
+
+  bool has_key(const Key& key) const {
+    return contain_key(ring_, key);
+  }
+
+  bool empty() const {
+    return ring_.empty();
+  }
+
+  /**
+   * \brief Get the responsible node for a client specified key.
+   */
+  Status get(const key_type& key, value_type* value) const {
+    key_type sep;
+    return get(key, &sep, value);
+  }
+
+  /// Gets both the sep and the value.
+  Status get(const key_type& key, key_type* sep, value_type* value) const {
+    CHECK_NOTNULL(sep);
+    CHECK_NOTNULL(value);
+    if (ring_.empty()) {
+      return Status(-ENOENT, "The ring is empty.");
+    }
+
+    auto it = ring_.upper_bound(key);
+    // if key is in the range between last key and first key, return the
+    // last element in the map.
+    if (it == ring_.begin() || it == ring_.end()) {
+      *sep = ring_.rbegin()->first;
+      *value = ring_.rbegin()->second;
+    } else {
+      --it;
+      *sep = it->first;
+      *value = it->second;
+    }
+    return Status::OK;
+  }
+
+  /**
+   * \brief return the partition starting points.
+   * \param[out] partitions A vector to stores the partitions;
+   *
+   * \TODO provides an iterator later.
+   */
+  void get_partitions(vector<key_type> *partitions) const {
+    CHECK_NOTNULL(partitions);
+    partitions->reserve(ring_.size());
+    append_keys_from_map(ring_, partitions);
+  }
+
+  /**
+   * \brief Gets all values.
+   * \TODO(lxu): Use iterator to avoid memory copies.
+   */
+  void get_values(vector<value_type> *values) const {
+    CHECK_NOTNULL(values);
+    auto iter = ring_.begin();
+    for (size_t i = 0; i < num_nodes(); ++i, ++iter) {
+      values->push_back(iter->second);
+    }
+  }
+
+  /**
+   * \brief Get the Max Range of the current ring.
+   * \param[out] range a (Key, Key) pair. If first key is smaller than the
+   * second key, then the range start clockwise from first key and ends
+   * at second key and does not cross zero. If first key is larger than
+   * the second key, then it is a range starting clockwise from first key
+   * and crossed zero to second key.
+   */
+  Status get_max_range(pair_type *key_pair) {
+    CHECK_NOTNULL(key_pair);
+    // if there is no element, return error.
+    if (ring_.empty()) {
+      return Status(-ENOENT, "Cannot get max range.");
+    }
+    // if there is only one element, set upper bound same as lower bound
+    // and let caller deal with it.
+    if (ring_.size() == 1) {
+      key_pair->first = key_pair->second = ring_.begin()->first;
+      return Status::OK;
+    }
+
+    auto it = ring_.begin();
+    key_pair->first = it->first;
+    key_pair->second = (++it)->first;
+    key_type max_range = key_pair->second - key_pair->first;
+    for (size_t i = 0; i < num_nodes(); i++) {
+      if (it == ring_.begin()) {
+        break;
+      }
+      key_type prev = it->first;
+      key_type range;
+      if (++it == ring_.end()) {
+        it = ring_.begin();
+      }
+      if (it->first > prev) {
+        range = it->first - prev;
+      } else {
+        // if it->first is the beginning now
+        range = (numeric_limits<Key>::max() - prev) + it->first;
+      }
+      if (range > max_range) {
+        max_range = range;
+        key_pair->first = prev;
+        key_pair->second = it->first;
+      }
+    }
+    return Status::OK;
+  }
+
+  /**
+   * \brief get the successive value of the value this is responsible
+   * for the given key.
+   * \param[in] key the current key specified by client.
+   * \param[out] successive the successive value returned to client.
+   */
+  Status succ(const Key& key, Value* successive) {
+    CHECK_NOTNULL(successive);
+    // O(logN)
+    auto it = ring_.find(key);
+    if (it == ring_.end()) {
+      return Status(-ENOENT, "The value is not in the ring.");
+    }
+    ++it;
+    if (it == ring_.end()) {
+      it = ring_.begin();
+    }
+    *successive = it->second;
+    return Status::OK;
+  }
+
+  /**
+   * \brief get the successive value of current value on the ring.
+   * \param[in] current the current value specified by client.
+   * \param[out] successive the successive value returned to client.
+   */
+  Status succ_by_value(const Value& current, Value* successive) {
+    CHECK_NOTNULL(successive);
+    // O(N)
+    auto it = ring_.begin();
+    for (; it != ring_.end(); ++it) {
+      if (it->second == current) {
+        break;
+      }
+    }
+    if (it == ring_.end()) {
+      return Status(-ENOENT, "The value is not in the ring.");
+    }
+    ++it;
+    // if current value is the last, then the next is the begin on the ring.
+    if (it == ring_.end()) {
+      it = ring_.begin();
+    }
+    *successive = it->second;
+    return Status::OK;
+  }
+
+  /**
+   * \brief get the previous value of the value that is responsible for
+   * the given key.
+   * \param[in] key the current key specified by client.
+   * \param[out] previous the previous value returned to client.
+   */
+  Status prev(const Key& key, Value* previous) {
+    CHECK_NOTNULL(previous);
+    // O(logN)
+    auto it = ring_.find(key);
+    if (it == ring_.end()) {
+      return Status(-ENOENT, "The key is not in the ring.");
+    }
+    if (it == ring_.begin()) {
+      *previous = ring_.rbegin()->second;
+    } else {
+      --it;
+      *previous = it->second;
+    }
+    return Status::OK;
+  }
+
+  /**
+   * \brief get the previous value of current value on the ring.
+   * \param[in] current the current value specified by client.
+   * \param[out] prev the previous value returned to client.
+   */
+  Status prev_by_value(const Value& current, Value* previous) {
+    CHECK_NOTNULL(previous);
+    // O(N)
+    auto it = ring_.begin();
+    for (; it != ring_.end(); ++it) {
+      if (it->second == current) {
+        break;
+      }
+    }
+    if (it == ring_.end()) {
+      return Status(-ENOENT, "The value is not in the ring.");
+    }
+    // if current value is the beginning, the next is the end on the ring.
+    if (it == ring_.begin()) {
+      *previous = ring_.rbegin()->second;
+    } else {
+      --it;
+      *previous = it->second;
+    }
+    return Status::OK;
+  }
+
+  /**
+   * \brief return a pair which contains a node and its range by the given key.
+   */
+  Status get_range(const Key& key, range_type* range) const {
+    CHECK_NOTNULL(range);
+    if (ring_.empty()) {
+      return Status(-ENOENT, "The ring is empty.");
+    }
+
+    pair_type range_pair;
+    auto it = ring_.upper_bound(key);
+    // if it's after the last element
+    if (it == ring_.end()) {
+      // lower bound is the last element
+      range_pair.first = ring_.rbegin()->first;
+      range->first = ring_.rbegin()->second;
+      // upper bound is the first element
+      if (ring_.begin()->first == 0) {
+        range_pair.second = numeric_limits<Key>::max();
+      } else {
+        range_pair.second = ring_.begin()->first - 1;
+      }
+    } else {
+      range_pair.second = it->first - 1;
+      range_pair.first = (--it)->first;
+      range->first = it->second;
+    }
+    range->second = range_pair;
+    return Status::OK;
+  }
+
+  /**
+   * \brief Gets the number of physical nodes.
+   */
+  size_t num_nodes() const {
+    return ring_.size() / num_partitions_per_node_;
+  }
+
+  /**
+   * \brief Gets the number of vnodes.
+   */
+  size_t num_partitions() const {
+    return ring_.size();
+  }
+
+  /**
+   * \brief Gets the number of partitions.
+   */
+  size_t num_partitions_per_node() const {
+    return num_partitions_per_node_;
+  }
+
+  iterator begin() {
+    return ring_.begin();
+  }
+
+  const_iterator begin() const {
+    return ring_.begin();
+  }
+
+  iterator end() {
+    return ring_.end();
+  }
+
+  const_iterator end() const {
+    return ring_.end();
+  }
+
+ private:
+  HashMap ring_;
+
+  size_t num_partitions_per_node_;
+};
+
+}  // namespace vobla
+
+#endif  // VOBLA_CONSISTENT_HASH_MAP_H_
---
+++ vobla/consistent_hash_map_test.cpp
@@ -0,0 +1,326 @@
+/**
+ * Copyright 2011-2013 (c) Ziling Huang <hzlgis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * \file consistent_hash_map_test.cpp
+ *
+ * \brief Unit test for Consistent Hashing class
+ *
+ * Copyright 2013 (c) Ziling Huang <hzlgis@gmail.com>
+ */
+
+#include <boost/lexical_cast.hpp>
+#include <gmock/gmock.h>
+#include <string.h>
+#include <limits>
+#include <string>
+#include <utility>
+#include <vector>
+#include "vobla/consistent_hash_map.h"
+#include "vobla/hash.h"
+#include "vobla/status.h"
+
+using boost::lexical_cast;
+using std::numeric_limits;
+using std::string;
+using std::vector;
+using ::testing::ContainerEq;
+
+namespace vobla {
+
+typedef ConsistentHashMap<size_t, string, 4> TestMap;
+
+TEST(ConsistentHashMapTest, TestInsert) {
+  TestMap test_map;
+  vector<size_t> partitions;
+
+  const string node1("node1");
+  SHA1Digest hash1(node1);
+  size_t hash_value1;
+  memcpy(&hash_value1, hash1.digest(), sizeof(size_t));
+  test_map.insert(hash_value1, node1);
+  EXPECT_EQ(static_cast<size_t>(4), test_map.num_partitions());
+  test_map.get_partitions(&partitions);
+  EXPECT_EQ(static_cast<size_t>(4), partitions.size());
+
+  const string node2("node2");
+  SHA1Digest hash2(node2);
+  size_t hash_value2;
+  memcpy(&hash_value2, hash2.digest(), sizeof(size_t));
+  test_map.insert(hash_value2, node2);
+  EXPECT_EQ(static_cast<size_t>(8), test_map.num_partitions());
+  partitions.clear();
+  test_map.get_partitions(&partitions);
+  EXPECT_EQ(static_cast<size_t>(8), partitions.size());
+}
+
+TEST(ConsistentHashMapTest, TestRemove) {
+  TestMap test_map;
+
+  const string node1("node1");
+  SHA1Digest hash1(node1);
+  size_t hash_value1;
+  memcpy(&hash_value1, hash1.digest(), sizeof(size_t));
+  test_map.insert(hash_value1, node1);
+
+  const string node2("node2");
+  SHA1Digest hash2(node2);
+  size_t hash_value2;
+  memcpy(&hash_value2, hash2.digest(), sizeof(size_t));
+  test_map.insert(hash_value2, node2);
+
+  test_map.remove(hash_value2);
+  EXPECT_EQ(static_cast<size_t>(4), test_map.num_partitions());
+  test_map.remove(hash_value1);
+  EXPECT_EQ(static_cast<size_t>(0), test_map.num_partitions());
+}
+
+TEST(ConsistentHashMapTest, TestGetPartitionOneNode) {
+  TestMap test_map;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  vector<size_t> expected_parts;
+  size_t range = numeric_limits<size_t>::max() / test_map.num_partitions();
+  for (unsigned int i = 0; i < test_map.num_partitions(); i++) {
+    expected_parts.push_back(range*i);
+  }
+  vector<size_t> actual_parts;
+  test_map.get_partitions(&actual_parts);
+  EXPECT_THAT(actual_parts, ContainerEq(expected_parts));
+}
+
+TEST(ConsistentHashMapTest, TestGetRange) {
+  TestMap test_map;
+  typedef std::pair<size_t, size_t> range_type;
+  typedef std::pair<string, range_type> node_range_type;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  size_t range = numeric_limits<size_t>::max() / test_map.num_partitions();
+  vector<size_t> expected_parts;
+  for (unsigned int i = 0; i < test_map.num_partitions(); i++) {
+    expected_parts.push_back(range*i);
+  }
+  vector<size_t> actual_parts;
+  test_map.get_partitions(&actual_parts);
+  EXPECT_THAT(actual_parts, ContainerEq(expected_parts));
+
+  range_type range1 = std::make_pair(0, range - 1);
+  range_type range2 = std::make_pair(range, range*2 - 1);
+  range_type range3 = std::make_pair(range*2, range*3 - 1);
+  range_type range4 = std::make_pair(range*3, numeric_limits<size_t>::max());
+  node_range_type range1_node1 = std::make_pair(node1, range1);
+  node_range_type range2_node1 = std::make_pair(node1, range2);
+  node_range_type range3_node1 = std::make_pair(node1, range3);
+  node_range_type range4_node1 = std::make_pair(node1, range4);
+  node_range_type tmp;
+  EXPECT_TRUE(test_map.get_range(range/2, &tmp).ok());
+  EXPECT_EQ(range1_node1, tmp);
+  EXPECT_TRUE(test_map.get_range(range + range/2, &tmp).ok());
+  EXPECT_EQ(range2_node1, tmp);
+  EXPECT_TRUE(test_map.get_range(range*2 + range/2, &tmp).ok());
+  EXPECT_EQ(range3_node1, tmp);
+  EXPECT_TRUE(test_map.get_range(range*3 + range/2, &tmp).ok());
+  EXPECT_EQ(range4_node1, tmp);
+}
+
+TEST(ConsistentHashMapTest, TestGetMaxRange) {
+  ConsistentHashMap<size_t, string, 1> test_map;
+  const string node1("node1");
+  const string node2("node2");
+  const string node3("node3");
+  const string node4("node4");
+  std::pair<size_t, size_t> range;
+
+  EXPECT_FALSE(test_map.get_max_range(&range).ok());
+
+  test_map.insert(10, node1);
+  EXPECT_TRUE(test_map.get_max_range(&range).ok());
+  EXPECT_EQ(range.first, static_cast<size_t>(10));
+  EXPECT_EQ(range.second, static_cast<size_t>(10));
+
+  test_map.insert(100, node2);
+  test_map.insert(500, node3);
+  EXPECT_TRUE(test_map.get_max_range(&range).ok());
+  EXPECT_EQ(range.first, static_cast<size_t>(500));
+  EXPECT_EQ(range.second, static_cast<size_t>(10));
+
+  test_map.insert(numeric_limits<size_t>::max() - 1, node4);
+  EXPECT_TRUE(test_map.get_max_range(&range).ok());
+  EXPECT_EQ(range.first, static_cast<size_t>(500));
+  EXPECT_EQ(range.second, numeric_limits<size_t>::max() - 1);
+}
+
+TEST(ConsistentHashMapTest, TestGet) {
+  TestMap test_map;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  const string node2("node2");
+  // insert another node in the middle of the first nodes's range
+  size_t hash_node2 = numeric_limits<size_t>::max() /
+                      test_map.num_partitions() / 2;
+  test_map.insert(hash_node2, node2);
+  size_t range_node = numeric_limits<size_t>::max() / test_map.num_partitions();
+  EXPECT_EQ(hash_node2, range_node);
+
+  string tmp;
+  for (int i = 0; i < 6; i++) {
+    if (i % 2 == 0) {
+      EXPECT_TRUE(test_map.get(range_node*i + range_node/2, &tmp).ok());
+      EXPECT_EQ(node1, tmp);
+    } else {
+      EXPECT_TRUE(test_map.get(range_node*i + range_node/2, &tmp).ok());
+      EXPECT_EQ(node2, tmp);
+    }
+  }
+}
+
+TEST(ConsistentHashMapTest, TestGetValues) {
+  TestMap test_map;
+  vector<string> expected_nodes;
+  for (int i = 0; i < 10; ++i) {
+    string node_name = string("node") + lexical_cast<string>(i);
+    test_map.insert(i * 1000, node_name);
+    expected_nodes.push_back(node_name);
+  }
+  vector<string> actual_nodes;
+  test_map.get_values(&actual_nodes);
+  EXPECT_THAT(actual_nodes, ContainerEq(expected_nodes));
+}
+
+TEST(ConsistentHashMapTest, TestSuccByValue) {
+  TestMap test_map;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  const string node2("node2");
+  test_map.insert(1000, node2);
+  const string node3("node3");
+  test_map.insert(2000, node3);
+
+  string tmp;
+  EXPECT_TRUE(test_map.succ_by_value(node1, &tmp).ok());
+  EXPECT_EQ(node2, tmp);
+}
+
+TEST(ConsistentHashMapTest, TestSucc) {
+  TestMap test_map;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  const string node2("node2");
+  test_map.insert(1000, node2);
+  const string node3("node3");
+  test_map.insert(2000, node3);
+
+  string tmp;
+  EXPECT_EQ(-ENOENT, test_map.succ(500, &tmp).error());
+  EXPECT_TRUE(test_map.succ(1000, &tmp).ok());
+  EXPECT_EQ(node3, tmp);
+  EXPECT_TRUE(test_map.succ(2000, &tmp).ok());
+  EXPECT_EQ(node1, tmp);
+}
+
+TEST(ConsistentHashMapTest, TestPrevByValue) {
+  TestMap test_map;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  const string node2("node2");
+  test_map.insert(1000, node2);
+  const string node3("node3");
+  test_map.insert(2000, node3);
+
+  string tmp;
+  EXPECT_TRUE(test_map.prev_by_value(node3, &tmp).ok());
+  EXPECT_EQ(node2, tmp);
+  EXPECT_TRUE(test_map.prev_by_value(node1, &tmp).ok());
+  EXPECT_EQ(node3, tmp);
+}
+
+TEST(ConsistentHashMapTest, TestPrev) {
+  TestMap test_map;
+  const string node1("node1");
+  test_map.insert(0, node1);
+  const string node2("node2");
+  test_map.insert(1000, node2);
+  const string node3("node3");
+  test_map.insert(2000, node3);
+
+  string tmp;
+  EXPECT_EQ(-ENOENT, test_map.prev(500, &tmp).error());
+  EXPECT_TRUE(test_map.prev(0, &tmp).ok());
+  EXPECT_EQ(node3, tmp);
+}
+
+TEST(ConsistentHashMapTest, TestCopyConstructor) {
+  TestMap test_map;
+  for (int i = 0; i < 10; i++) {
+    test_map.insert(0*100, string("node") + lexical_cast<string>(i));
+  }
+  vector<TestMap::key_type> partitions;
+  test_map.get_partitions(&partitions);
+
+  TestMap copy_map = test_map;
+  vector<TestMap::key_type> copy_partitions;
+  copy_map.get_partitions(&copy_partitions);
+  EXPECT_EQ(partitions, copy_partitions);
+}
+
+TEST(ConsistentHashMapTest, TestIterator) {
+  TestMap test_map;
+  for (int i = 0; i < 10; i++) {
+    test_map.insert(i*100, string("node") + lexical_cast<string>(i));
+  }
+
+  TestMap::iterator it = test_map.begin();
+  for (size_t i = 0; i < test_map.num_nodes(); ++i, ++it) {
+    EXPECT_EQ(i*100, it->first);
+    EXPECT_EQ(string("node") + lexical_cast<string>(i), it->second);
+  }
+
+  TestMap::const_iterator cit = test_map.begin();
+  for (size_t i = 0; i < test_map.num_nodes(); ++i, ++cit) {
+    EXPECT_EQ(i*100, cit->first);
+    EXPECT_EQ(string("node") + lexical_cast<string>(i), cit->second);
+  }
+}
+
+TEST(ConsistentHashMapTest, TestSinglePartition) {
+  ConsistentHashMap<uint64_t, string, 1> test_map;
+  test_map.insert(0, "p0");
+  uint64_t sep = std::numeric_limits<uint64_t>::max() / 2;
+  test_map.insert(sep, "p1");
+}
+
+TEST(ConsistentHashMapTest, TestNumNodes) {
+  TestMap test_map;
+  for (int i = 0; i < 10; i++) {
+    test_map.insert(i*100, string("node") + lexical_cast<string>(i));
+  }
+  EXPECT_EQ(10, test_map.num_nodes());
+}
+
+TEST(ConsistentHashMapTest, TestNumPartitions) {
+  TestMap test_map;
+  for (int i = 0; i < 10; i++) {
+    test_map.insert(i*100, string("node") + lexical_cast<string>(i));
+  }
+  EXPECT_EQ(10*4, test_map.num_partitions());
+}
+
+TEST(ConsistentHashMapTest, TestNumPartitionsPerNode) {
+  TestMap test_map;
+  EXPECT_EQ(4, test_map.num_partitions_per_node());
+}
+
+}  // namespace vobla
